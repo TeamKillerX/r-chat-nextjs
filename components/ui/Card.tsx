a5d95c378fbabf5c584ef5c07ae19461af0b8a27
Randy SL <killerx@randydev.my.id>
2025-09-24 18:19:43 +0700

    Create Card.tsx

---
+++ components/ui/Card.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { cn } from '@/lib/utils'
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        'rounded-lg bg-gray-900/80 backdrop-blur-xl shadow-md',
+        className
+      )}
+      {...props}
+    />
+  )
+)
+
+Card.displayName = 'Card'
+
+export { Card }
